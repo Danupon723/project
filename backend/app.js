48adf1dc2d48fd0e12ba24e5e2a135f9728ef4cb
tarzan2550t <tarzan2550t@gmail.com>
2025-11-16 11:40:10 +0700

    add morgan and cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,26 @@
-const express = require('express');
 require('dotenv').config()
+
+const express = require('express');
+const cors = require('cors')
+const morgan = require('morgan')
+
 const auth = require('./router/authrouter');
-
 const app = express(); 
+app.use(cors({origin:process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : '*'}))
+    
 
+app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
+app.use(morgan('dev'))
+
+app.get('/oak' , (req,res)=>{
+    console.log('req = ' , req.query)
+    res.json({
+        status:"hello world oak" , 
+        data : req.query
+    })
+})
+
 app.use('/api/auth' , auth )
 
 module.exports = app
