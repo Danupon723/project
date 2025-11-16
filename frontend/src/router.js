caac65bf1e87477e4d673f69a019456cc7c0bd8f
tarzan2550t <tarzan2550t@gmail.com>
2025-11-16 14:45:09 +0700

    create router vue

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,36 @@
 import {createRouter,creatWebhistory} from 'vue-router'
 import login from './view/login.vue'
+import adminIndex from './view/admin/index.vue'
+import admindashbord from './view/admin/dashbord.vue'
+import adminuser from './view/admin/users.vue'
+import { path } from '../../backend/app'
+import { Children, Component } from 'react'
 
 
 const routes = [
-    {path:'login' , Component:login},
+    {
+        path:'/' ,
+        name: login , 
+        Component:login
+    },
+    {
+        path:'/admin',
+        Component:adminIndex , 
+        meta: {requiresAuth : true} , 
+        Children:[
+            {
+            path:'dashbord' , 
+            name : 'admindashbord' ,
+            component: admindashbord
+            } , 
+            {
+            path:'user' , 
+            name : 'user' ,
+            component: adminuser
+            } , 
+
+    ]
+    }
 ]
 
 
