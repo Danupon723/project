23c24535449c551608c7c71d03fedc7fab069d42
tarzan2550t <tarzan2550t@gmail.com>
2025-11-16 13:55:38 +0700

    create router function  createuser

--- backend/router/admin.router.js
+++ backend/router/admin.router.js
@@ -2,5 +2,6 @@ const router  = require('express').Router()
 const admincontroller = require('../controller/admin.controller')
 
 router.get('/userslist' , admincontroller.userlist)
+router.post('/createuser' , admincontroller.createuser)
 
 module.exports = router
