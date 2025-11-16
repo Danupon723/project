12702ce641bd19bae3cdff4873c71a2de6b1a344
tarzan2550t <tarzan2550t@gmail.com>
2025-11-16 14:00:01 +0700

    edit collum insert database org_group_id

--- backend/controller/admin.controller.js
+++ backend/controller/admin.controller.js
@@ -21,7 +21,7 @@ exports.createuser = async (req,res,next) =>{
 
         const password_hash = await bcrypt.hash(password , 10)
 
-        const create = await conn('users').insert({name,email,password:password_hash,department_id: department ,org_group_id : group_id,role})
+        const create = await conn('users').insert({name,email,password:password_hash,department_id: department ,org_groups_id: group_id,role})
             res.json({success:true , message: 'create user complata'})
         
 
